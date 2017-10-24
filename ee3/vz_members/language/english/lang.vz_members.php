9959564771d648da7ede09aad3a782f98c997d1e
Jasper van Es <jasper@0-to-9.nl>
2017-10-24 13:29:29 +0200

    Start of ee3 compatible version

---
+++ ee3/vz_members/language/english/lang.vz_members.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang = array(
+
+'mode_label' => 'Selection mode:',
+'mode_label_cell' => 'Mode',
+'mode_single' => 'Allow only one member (drop-down)',
+'mode_multiple' => 'Allow multiple members (checkboxes)',
+'mode_multiselect' => 'Allow multiple members (multi-select)',
+
+'member_groups_label' => 'Allow members from these groups:',
+'member_groups_label_cell' => 'Allowed groups',
+
+'no_member_groups' => 'There are no member groups selected',
+
+''=>''
+);
+
+/* End of file lang.vz_members.php */
